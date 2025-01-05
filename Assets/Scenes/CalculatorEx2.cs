80c39fc199ba5410742ac85184169b93d00e21ab
ordegani <oradeganii@gmail.com>
2025-01-05 17:58:07 +0200

    Disable button

--- Assets/Scenes/CalculatorEx2.cs
+++ Assets/Scenes/CalculatorEx2.cs
@@ -8,6 +8,7 @@ public class CalculatorWithEvent : MonoBehaviour
     [SerializeField] private TMP_InputField _secondInputField;//I renamed to _secondInputField
     [SerializeField] private TextMeshProUGUI _resultField;
     [SerializeField] private Button _calculateButton;
+    [SerializeField] private Button _calculateButtondisabler;
     [SerializeField] private TMP_Dropdown _operatorsDropdown;
     private float _result;//I changed to private
 
@@ -19,7 +20,7 @@ public class CalculatorWithEvent : MonoBehaviour
 
     private void OnDisable()
     {
-        _calculateButton.onClick.RemoveListener(HandleCalculateButtonClick);
+        _calculateButtondisabler.onClick.RemoveListener(HandleCalculateButtonClick);
     }
 
     private void HandleCalculateButtonClick()//I renamed to HandleCalculateButtonClick().n event-driven frameworks, it's common to name event handlers with a verb and context (e.g., OnButtonClick, HandleEvent). "handle" is a convention for event being handled, "calculateButton" is the ui object that is pressed by the user. "click" is the user's action that triggers the event-the most important part.Explains it handles a button click.
