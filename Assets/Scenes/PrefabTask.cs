bdf51aaa44db7756d1ce86ffe560db662c781602
ordegani <oradeganii@gmail.com>
2025-01-08 22:47:18 +0200

    prefab comments addition

--- Assets/Scenes/PrefabTask.cs
+++ Assets/Scenes/PrefabTask.cs
@@ -12,14 +12,17 @@ public class PrefabTask : MonoBehaviour
     {
         HandlMouseClick();
     }
-    // Detect left mouse button click and return the click location
+
     void HandlMouseClick()
     {
+        // Detect left mouse button click and return the click location
         if (Input.GetMouseButtonDown(0) && i <= 9)
         {
             _mousePos = Input.mousePosition;
             Debug.Log("mouse position (x,y) is:" + _mousePos.x + "," + _mousePos.y);
+            //Instantiate _myPrefab and make it's position _mousePos
             Instantiate(_myPrefab, _mousePos, Quaternion.identity);
+            //add to count to hadle 10 prefabs  instantiate limit in this task
             i++;
             //Debug.Log(_myPrefab.transform.position);
         }
