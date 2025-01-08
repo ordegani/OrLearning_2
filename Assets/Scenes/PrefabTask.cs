1579cc2fdb1b2c2b7f82e640f3234c381aec236e
ordegani <oradeganii@gmail.com>
2025-01-08 22:27:55 +0200

    prefab fix

--- Assets/Scenes/PrefabTask.cs
+++ Assets/Scenes/PrefabTask.cs
@@ -6,21 +6,22 @@ public class PrefabTask : MonoBehaviour
 {
     [SerializeField] private GameObject _myPrefab;
     private Vector3 _mousePos;
-    private int i;
+    private int i = 0;
 
-void Update()
+    void Update()
     {
-        // Detect left mouse button click and return the click location
-        if (Input.GetMouseButtonDown(0))
+        HandlMouseClick();
+    }
+    // Detect left mouse button click and return the click location
+    void HandlMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0) && i < 9)
         {
-            Debug.Log("Left mouse button clicked!");
             _mousePos = Input.mousePosition;
             Debug.Log("mouse position (x,y) is:" + _mousePos.x + "," + _mousePos.y);
+            Instantiate(_myPrefab, _mousePos, Quaternion.identity);
             i++;
-        }
-        while(i<9)
-        {
-            Instantiate(_myPrefab, new Vector3(_mousePos.x, _mousePos.y, 0), Quaternion.identity);
+            //Debug.Log(_myPrefab.transform.position);
         }
     }
 }
