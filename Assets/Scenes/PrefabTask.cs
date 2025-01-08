07e313bf3a2eec66233f4bf9ac4cd36b23cc754f
ordegani <oradeganii@gmail.com>
2025-01-08 22:42:15 +0200

    prefab fix

--- Assets/Scenes/PrefabTask.cs
+++ Assets/Scenes/PrefabTask.cs
@@ -15,7 +15,7 @@ public class PrefabTask : MonoBehaviour
     // Detect left mouse button click and return the click location
     void HandlMouseClick()
     {
-        if (Input.GetMouseButtonDown(0) && i < 9)
+        if (Input.GetMouseButtonDown(0) && i <= 9)
         {
             _mousePos = Input.mousePosition;
             Debug.Log("mouse position (x,y) is:" + _mousePos.x + "," + _mousePos.y);
