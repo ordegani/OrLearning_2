963129ccb7b3b72944552559510b6745382e969b
ordegani <oradeganii@gmail.com>
2025-01-26 16:43:59 +0200

    develop

--- Assets/Scenes/PrefabTask.cs
+++ Assets/Scenes/PrefabTask.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PrefabTask : MonoBehaviour
 {
     [SerializeField] private GameObject _myPrefab;
+    [SerializeField] private Canvas _canvas;
     private Vector3 _mousePos;
     private int _myPrefabCount = 0;
 
@@ -21,7 +23,7 @@ public class PrefabTask : MonoBehaviour
             _mousePos = Input.mousePosition;
             Debug.Log("mouse position (x,y) is:" + _mousePos.x + "," + _mousePos.y);
             //Instantiate _myPrefab and make it's position _mousePos
-            Instantiate(_myPrefab, _mousePos, Quaternion.identity);
+            Instantiate(_myPrefab, _mousePos, Quaternion.identity, _canvas.transform);
             //add to count to hadle 10 prefabs  instantiate limit in this task
             _myPrefabCount++;
             //Debug.Log(_myPrefab.transform.position);
