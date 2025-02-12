029d8a471deec94d0f94963e7b5d02c7958a34cd
ordegani <oradeganii@gmail.com>
2025-02-12 18:27:52 +0200

    prefab D3 with screenPointToRay

--- Assets/Scenes/NewBehaviourScript.cs
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -1,43 +1,47 @@
 
-using System.Collections.Generic; // todo: you don't need them yet maybe
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour //3D
 {
     [SerializeField] private GameObject _myPrefab;
-    [SerializeField] private Camera _mainCamera;
     private int _myPrefabCount = 0;
+    private int _maxPrefabs = 8;
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            handleMouseClick2();
+            HandleMouseClick();
         }
     }
 
-    //One solution (less priffered in this specific assigment, but good for spawning an obj in front of an existing obj)>>
-    private void HandlMouseClick()
+    //One solution(Good for spawning an obj in front of an existing obj) Good for managing user's inputs>>
+    private void HandleMouseClick()
     {
-        Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        if (_myPrefabCount <= 9 && Physics.Raycast(ray, out hit))
+        if (_myPrefabCount <= _maxPrefabs && Physics.Raycast(ray, out hit))
         {
-            Instantiate(_myPrefab, hit.point, Quaternion.identity);
+            Instantiate(_myPrefab,hit.point , Quaternion.identity);
             Debug.Log(hit.collider.name);
             Debug.Log(hit.point);
             _myPrefabCount++;
         }
     }
 
-    //Second solution>>
-    private void handleMouseClick2()
+    //Second solution(Good for spawning an obj in general) Good for creating the gam's background>>
+    private void HandleMouseClick2()
     {
-        Vector3 touchPos = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        Instantiate(_myPrefab, touchPos, Quaternion.identity);
-        Debug.Log(touchPos);
-
+        if (_myPrefabCount < _maxPrefabs)
+        {
+            //Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+            Instantiate(_myPrefab, point, Quaternion.identity);
+            Debug.Log(point);
+            _myPrefabCount++;
+        }
     }
 }
