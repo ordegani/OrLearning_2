6719cb5979cff461697133473a1b0c3633612459
ordegani <oradeganii@gmail.com>
2025-02-21 18:01:24 +0200

    object pooling

--- Assets/Scenes/ObjectPooling.cs
+++ Assets/Scenes/ObjectPooling.cs
@@ -3,36 +3,56 @@ using UnityEngine;
 
 public class ObjectPooling : MonoBehaviour
 {
-    [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private List<GameObject> bulletPool = new List<GameObject>();
-    private int poolSize = 10;
+    //[SerializeField] private Material _someMaterial;
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private List<GameObject> _bulletPool = new List<GameObject>();
+    private int _poolSize = 10;
     Vector3 position;
 
-    void Start()
+    private void Start()
     {
-        for (int i = 0; i < poolSize; i++)
+        //create pool
+        for (int i = 0; i < _poolSize; i++)
         {
-            GameObject bullet = Instantiate(bulletPrefab);
+            GameObject bullet = Instantiate(_bulletPrefab);
             bullet.SetActive(false);
-            bulletPool.Add(bullet);
+            _bulletPool.Add(bullet);
         }
     }
 
-    public GameObject GetBullet()
+    private void Update()
     {
-        foreach (GameObject bullet in bulletPool)
+        GetBullet();
+    }
+    //function to use to instantiate new visible bullet in the game view
+    private void HandleMouseClick()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Color color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject newPrefab = Instantiate(_bulletPrefab, hit.point, Quaternion.identity);
+        }
+    }
+
+    //use bullet from pool
+    private GameObject GetBullet()
+    {
+        foreach (GameObject bullet in _bulletPool)
         {
             if (!bullet.activeInHierarchy) // Find an inactive bullet
             {
-                bullet.transform.position = position;
+                HandleMouseClick();
                 bullet.SetActive(true);
                 return bullet;
             }
         }
 
         // No inactive bullets available, create a new one (optional)
-        GameObject newBullet = Instantiate(bulletPrefab, position, Quaternion.identity);
-        bulletPool.Add(newBullet);
+        GameObject newBullet = Instantiate(_bulletPrefab, position, Quaternion.identity);
+        _bulletPool.Add(newBullet);
         return newBullet;
     }
 }
